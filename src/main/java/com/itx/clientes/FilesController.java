1c8a162cdc35dcf06427805aae5c693e5b08a2db
hernan2104 <christian2104@yahoo.com>
2023-11-15 22:17:10 -0300

    Modificacion4

--- src/main/java/com/itx/clientes/FilesController.java
+++ src/main/java/com/itx/clientes/FilesController.java
@@ -1,9 +1,14 @@
 package com.itx.clientes;
 
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FilesController {
@@ -16,4 +21,27 @@ public class FilesController {
 
         return "welcome"; //view
     }
+    
+    @PostMapping("/upload-csv")
+    public String upload(@RequestParam("file") MultipartFile file, 
+    		@RequestParam(value = "optAdd", required = false) String opAdd, 
+    		@RequestParam(value = "typeContactAdd", required = false) String typeContact,
+    		Model model) {
+    	
+        try {
+            if (file.isEmpty()) {
+                  model.addAttribute("message", "Seleccione el archivo para importar los contacto.");
+                  model.addAttribute("status", false);
+            }
+            else {
+      		  model.addAttribute("status", true);
+            }
+        }
+        catch (Exception  e) {
+          	 model.addAttribute("status", false);
+           	 model.addAttribute("message",e.getMessage());
+        }
+        return "file-upload-status";
+
+    }    
 }
